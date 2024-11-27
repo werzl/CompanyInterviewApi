99f0151a188d61e7223645c34441d4dd04137d0f
Adam Hewitt <adam@houseofhewitt.co.uk>
2024-11-27 16:29:17 +0000

    Update ICommand.cs

--- Api/Commands/ICommand.cs
+++ Api/Commands/ICommand.cs
@@ -14,16 +14,18 @@ public class CommandResult
         _errors = errors;
     }
 
-    public bool IsSuccessful => !_errors.Any();
+    public bool IsSuccessful { get; private set; }
     public IEnumerable<string> Errors => _errors;
 
     public static CommandResult Succeed()
     {
+        IsSuccessful = true;
         return new CommandResult();
     }
 
     public static CommandResult Fail(params string[] errors)
     {
+        IsSuccessful = false;
         return new CommandResult(errors);
     }
 }
